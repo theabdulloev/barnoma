99b6f8daab4fe1f53c0fc85163b200b24390464c
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2023-12-18 16:11:22 +0500

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+    <Html lang="en">
+        <Head>
+            <Title>Barnomasozi TJ</Title>
+        </Head>
+
+        <Body class="">
+            <NuxtLayout>
+                <NuxtPage />
+            </NuxtLayout>
+            <UNotifications />
+        </Body>
+    </Html>
+</template>
+
+<script setup lang="ts"></script>
